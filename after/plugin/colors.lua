3767f9c4bfd99d756a3a2385c8590872c33f7157
Daniel Bluhm <dbluhm@pm.me>
2023-12-01 00:47:30 -0500

    comments on colors
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,31 +1,31 @@
 local colors = {
-  base00 = "#1d1f21",
-  base01 = "#282a2e",
-  base02 = "#373b41",
-  base03 = "#969896",
-  base04 = "#b4b7b4",
-  base05 = "#c5c8c6",
-  base06 = "#e0e0e0",
-  base07 = "#ffffff",
-  base08 = "#cc6666",
-  base09 = "#de935f",
-  base0A = "#f0c674",
-  base10 = "#f0c674",
-  base0B = "#b5bd68",
-  base11 = "#b5bd68",
-  base0C = "#8abeb7",
-  base12 = "#8abeb7",
-  base0D = "#81a2be",
-  base13 = "#81a2be",
-  base0E = "#b294bb",
-  base14 = "#b294bb",
-  base0F = "#a3685a",
-  base15 = "#a3685a",
+  base00 = "#1d1f21", -- bg
+  base01 = "#282a2e", -- bg lighter
+  base02 = "#373b41", -- bg ligter still
+  base03 = "#969896", -- gray
+  base04 = "#b4b7b4", -- gray lighter
+  base05 = "#c5c8c6", -- fg
+  base06 = "#e0e0e0", -- fg lighter
+  base07 = "#ffffff", -- fg bright
+  base08 = "#cc6666", -- red
+  base09 = "#de935f", -- orange
+  base0A = "#f0c674", -- yellow
+  base10 = "#f0c674", -- yellow
+  base0B = "#b5bd68", -- green
+  base11 = "#b5bd68", -- green
+  base0C = "#8abeb7", -- cyan
+  base12 = "#8abeb7", -- cyan
+  base0D = "#81a2be", -- blue
+  base13 = "#81a2be", -- blue
+  base0E = "#b294bb", -- purple
+  base14 = "#b294bb", -- purple
+  base0F = "#a3685a", -- brown
+  base15 = "#a3685a", -- brown
 }
 
+local hl = vim.api.nvim_set_hl
 function Colors()
   vim.cmd.colorscheme("base16-tomorrow-night")
-  local hl = vim.api.nvim_set_hl
   vim.cmd('colorscheme base16-tomorrow-night')
   hl(0, "DiagnosticError", {bold = true, fg = colors.base08})
   hl(0, "DiagnosticWarn", {fg = colors.base09})
@@ -44,7 +44,7 @@ function Colors()
   hl(0, 'DiagnosticsUnderlineInformation', {undercurl=true, sp = colors.base0D})
   hl(0, 'DiagnosticsUnderlineHint', {undercurl=true, sp = colors.base05})
   hl(0, "@variable", {fg = colors.base05})
-  hl(0, "@attribute", {italic=true})
+  hl(0, "@attribute", {fg = colors.base14, italic=true})
   hl(0, "@keyword", {fg = colors.base14, italic=true})
 
   local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
